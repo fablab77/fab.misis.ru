38691887b334ddf46c6dbb6b1ad73b1913f203ec
Eugen Soloviov <suenot@gmail.com>
2015-05-16 23:30:11 +0300

    jq google form

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
 	$('.parallax').parallax();
 	$('.scrollspy').scrollSpy();
+
+	var googleForm = $(window).jqGoogleForms({"formKey": "1-TcT0uIi8RlMqInjvKXKCcCwejKcI6bHBHskjqnP2XU"});
 	$('.request-form').submit(function(e) {
 		e.preventDefault();
+		var email = $(this).find('#email').val();
+		googleForm.sendFormData({
+			"entry.1697207204": email
+		});
 		Materialize.toast('Заявка отправлена!', 4000);
 	});
 });
