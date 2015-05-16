4e16a5834dd4ac881962817a554f14beb08151c9
Eugen Soloviov <suenot@gmail.com>
2015-05-16 23:13:22 +0300

    request

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
 	$('.parallax').parallax();
 	$('.scrollspy').scrollSpy();
+	$('.request-form').submit(function(e) {
+		e.preventDefault();
+		Materialize.toast('Заявка отправлена!', 4000);
+	});
 });
